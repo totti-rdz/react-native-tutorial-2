34f3e0291e9ffb6115c19e1406b59c9144d7320b
totti-rdz <thorsten@achtsamkeitsakademie.de>
2022-10-19 22:36:34 +0200

    add shadow to StartGameScreen

--- src/screens/StartGameScreen.jsx
+++ src/screens/StartGameScreen.jsx
@@ -15,5 +15,16 @@ const StartGameScreen = () => {
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-  inputContainer: {marginTop: 100, marginHorizontal: 24, padding: 16, backgroundColor: "#72063c", borderRadius: 8},
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
 });
