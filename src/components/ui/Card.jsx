f1a75e24498d2776c93d87d187c7ddb8da772025
totti-rdz <thorsten@achtsamkeitsakademie.de>
2022-10-30 20:04:21 +0100

    use dimensions of "window" instead of "screen"

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({ children }) => {
 
 export default Card;
 
-const deviceWidth = Dimensions.get("screen").width;
+const deviceWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
   inputContainer: {
